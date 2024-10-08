45a99dbcf2c3d1ce9f4c8731cae8a6ff2f9ea30a
Arthur Henrique de carvalho <141870749+zekapalgordinho@users.noreply.github.com>
2024-10-08 12:19:41 -0300

    ss

---
+++ perguntas.js
@@ -0,0 +1,87 @@
+criaCartao{
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q/2'
+}
+criaCartao(
+'história',
+'Que ano o Brasil foi descorbeto?',
+'1500'
+)
+criaCartao(
+     'Português',
+    'O que é adjtivo?',
+    'Adjetivo é toda palavra que caracteriza o substantivo, indicando-lhe qualidade, defeito, estado, condição, etc.'
+)
+criaCartao(
+    'Química',
+    'O que é Nox?',
+    'O NOX é a carga que um elemento adquire depois de realizar qualquer tipo de ligação para atingir a estabilidade (regra do octeto) ou de se manter no seu estado fundamental.',
+
+)
+criaCartao(
+    'Biologia',
+    'O que é cromossomo?',
+    'Cromossomos são estruturas que contêm o DNA, armazenando a informação genética. Em humanos, há 46 cromossomos (23 pares). Eles são essenciais para a divisão celular.',
+
+)
+criaCartao(
+    'Português',
+    'O que é Substantivo?',
+    'Substantivo é a classe de palavras usada para dar nome aos seres, objetos, fenômenos, lugares, qualidades, ações, dentre outros.',
+
+)
+criaCartao(
+    'Português',
+    'O que é Verbo?',
+    'Um verbo é uma palavra que indica acontecimentos representados no tempo, como uma ação, um estado, um processo ou um fenômeno. ',
+
+)
+criaCartao(
+    'Português',
+    'O que é Pronone?',
+    'Pronomes são vocábulos que substituem ou acompanham um termo substantivo.',
+
+)
+criaCartao(
+    'Português',
+    'O que é uma charge?',
+    'Charge é um gênero textual jornalístico que tem como intenção principal fazer uma crítica sobre algum tema usando o humor.',
+
+)
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Fotossíntese é um processo realizado pelas plantas para a produção de energia necessária para a sua sobrevivência',
+
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Angola?',
+    'Luanda',
+
+)
+criaCartao(
+    'Geografia',
+    'O que uma Ilha?',
+    'Uma ilha é qualquer pedaço de terra subcontinental cercada por água.',
+
+)
+criaCartao(
+    'Astranomia',
+    'O que é um Planeta?',
+    'Um planeta é um corpo celeste que orbita uma estrela, tem massa suficiente para que sua gravidade o torne esférico e não é uma estrela nem um satélite.',
+
+)
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2?',
+    '4',
+
+)
+criaCartao(
+    'Matemática',
+    'Quanto é 3.3?',
+    '9',
+    
+)
