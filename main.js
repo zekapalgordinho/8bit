0859147386d39ad1b83de3304deb90208e6fd293
Arthur Henrique de carvalho <141870749+zekapalgordinho@users.noreply.github.com>
2024-10-15 11:46:41 -0300

    m

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function criaCartao (categoria, pergunta, resposta){
-    let container = document.getElementById('conteiner');
+    let container = document.getElementById('container');
     let cartao = document.createElement('article')
 cartao.className ='cartao';
 cartao.innerHTML =`
