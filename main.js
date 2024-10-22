63b6d8040db97f872b91a628462165bc531ba822
Arthur Henrique de carvalho <141870749+zekapalgordinho@users.noreply.github.com>
2024-10-22 19:06:12 -0300

    m

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article')
-cartao.className ='cartao';
-cartao.innerHTML =`
-<div class = "conteudo-cartao">
+cartao.className = 'cartao';
+cartao.innerHTML = `
+<div class="conteudo-cartao">
 <h3>${categoria}</h3>
 <div class = "pergunta-cartao">
 <p>${pergunta}</p>
@@ -13,6 +13,4 @@ cartao.innerHTML =`
 </div>
 </div>
 `
-container.appendChild(cartao);
-
 }
