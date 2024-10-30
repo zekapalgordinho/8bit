b0d58d9e4bbbe13134ed6b064568a1618741a35f
Arthur Henrique de carvalho <141870749+zekapalgordinho@users.noreply.github.com>
2024-10-30 12:02:00 -0300

    k

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ cartao.innerHTML = `
     </div>
 </div>
 `
-container.appendChild(cartao)
 
+let respostaEstaVisivel = false;
+
+function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel);
+}
+
+cartao.addEventListener('click', viraCartao);
+
+container.appendChild(cartao)
 }
