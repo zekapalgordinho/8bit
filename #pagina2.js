0c1a08b1a4d30d484421b1f18cd4f684f3a91675
Arthur Henrique de carvalho <141870749+zekapalgordinho@users.noreply.github.com>
2024-11-06 11:51:53 -0300

    gj

---
+++ #pagina2.js
@@ -0,0 +1,28 @@
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article')
+cartao.className = 'cartao';
+cartao.innerHTML = `
+<div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class = "pergunta-cartao">
+        <p>${pergunta}</p>
+    </div>
+    <div class= "resposta-cartao">
+        <p>${resposta}</p>
+    </div>
+</div>
+`
+  
+container.appendChild(cartao)
+}
+
+function mostraReposta(card){
+    const resposta =card.querySelector('resposta-cartao');
+    if(resposta.style.display ==='none'){
+        resposta.style.display = 'block';    
+}
+else{
+    resposta.style.display = 'none';
+}
+}
